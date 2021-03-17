58973c7f0cddd5397587eb4a3b722281d3c42cb0
Magician_Legion-R7 <kunkun8824@163.com>
2021-03-17 18:32:28 +0800

    2021-03-17 V2.0 init

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Rayplus_Axure
-Rayplus系统影像部分原型
+**Rayplus系统影像部分原型**
+@ Copyright 2021 by Magician
+
+注意事项：
+
+1. 使用谷歌浏览器预览，若网页无法正常打开，请安装[插件](https://chrome.google.com/webstore/detail/axure-rp-extension-for-ch/dogkpdfcklifaemcdfbildhcofnopogp)
+2. 图标使用阿里巴巴矢量图标库，部分图标可见[//at.alicdn.com/t/font_2427188_jy80xzh2apr.css](//at.alicdn.com/t/font_2427188_jy80xzh2apr.css)（保持更新！）
+
+# 医学影像系统_原型V1.0
+
+按照7个模块分别设计对应面板
+
+# 医学影像系统_原型V2.0
+
+主要分为两个页面：文件管理及阅片编辑，其余功能由更多功能按钮键进入
+
+## 2021-03-17 
+
+Add：完成文件管理及基本阅片编辑页面
+
+Need：尚未完善图标功能，如视图显示
---
+++ 医学影像系统_原型V1.0/files/功能分析（旧）/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,_(g,h,i,_(j,k,l,m)),n,[],o,_(h,p),q,[r,s,t],u,_(v,w,x,y,g,z,A,_(),B,[],C,_(D,E,F,G,H,_(I,J,K,L),M,null,N,O,O,P,Q,R,null,S,T,U,V,W,X,Y,T,Z,ba,_(I,J,K,bb),bc,T,bd,be,_(bf,bg,bh,bi,bj,bi,bk,bi,bl,m,K,_(bm,bn,bo,bn,bp,bn,bq,br)),i,_(j,k,l,m)),bs,_(),bt,_(),bu,_(bv,[])),bw,_(),bx,_());}; 
+var b="url",c="功能分析（旧）.html",d="generationDate",e=new Date(1615976189559.38),f="defaultAdaptiveView",g="name",h="",i="size",j="width",k=1024,l="height",m=0,n="adaptiveViews",o="sketchKeys",p="s0",q="variables",r="OnLoadVariable",s="currentTime",t="Ellipsoid",u="page",v="packageId",w="dae30e5b934d4dbfbdd437c6b0f52ab5",x="type",y="Axure:Page",z="功能分析（旧）",A="notes",B="annotations",C="style",D="baseStyle",E="627587b6038d43cca051c114ac41ad32",F="pageAlignment",G="center",H="fill",I="fillType",J="solid",K="color",L=0xFFFFFFFF,M="image",N="imageAlignment",O="near",P="imageRepeat",Q="auto",R="favicon",S="sketchFactor",T="0",U="colorStyle",V="appliedColor",W="fontName",X="Applied Font",Y="borderWidth",Z="borderVisibility",ba="borderFill",bb=0xFF797979,bc="cornerRadius",bd="cornerVisibility",be="outerShadow",bf="on",bg=false,bh="offsetX",bi=5,bj="offsetY",bk="blurRadius",bl="spread",bm="r",bn=0,bo="g",bp="b",bq="a",br=0.349019607843137,bs="adaptiveStyles",bt="interactionMap",bu="diagram",bv="objects",bw="masters",bx="objectPaths";
+return _creator();
+})());
