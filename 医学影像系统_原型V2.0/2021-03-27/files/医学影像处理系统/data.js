b2a6c73afe533a7d5a570a0ce3b6693baf546552
Magician_Legion-R7 <kunkun8824@163.com>
2021-03-27 15:57:02 +0800

    2021-03-27 完善视图、上传等功能

---
+++ 医学影像系统_原型V2.0/2021-03-27/files/医学影像处理系统/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,_(g,h,i,_(j,k,l,m)),n,[],o,_(h,p),q,[r,s,t],u,_(v,w,x,y,g,z,A,_(),B,[],C,_(D,E,F,G,H,_(I,J,K,L),M,null,N,G,G,O,P,Q,null,R,S,T,U,V,W,X,S,Y,Z,_(I,J,K,ba),bb,S,bc,bd,_(be,bf,bg,bh,bi,bh,bj,bh,bk,m,K,_(bl,bm,bn,bm,bo,bm,bp,bq)),i,_(j,k,l,m)),br,_(),bs,_(),bt,_(bu,[])),bv,_(),bw,_());}; 
+var b="url",c="医学影像处理系统.html",d="generationDate",e=new Date(1616831418809.67),f="defaultAdaptiveView",g="name",h="",i="size",j="width",k=1024,l="height",m=0,n="adaptiveViews",o="sketchKeys",p="s0",q="variables",r="OnLoadVariable",s="currentTime",t="Ellipsoid",u="page",v="packageId",w="179420c3eae94a3c8088d8309c093c99",x="type",y="Axure:Page",z="医学影像处理系统",A="notes",B="annotations",C="style",D="baseStyle",E="627587b6038d43cca051c114ac41ad32",F="pageAlignment",G="center",H="fill",I="fillType",J="solid",K="color",L=0xFFFFFFFF,M="image",N="imageAlignment",O="imageRepeat",P="auto",Q="favicon",R="sketchFactor",S="0",T="colorStyle",U="appliedColor",V="fontName",W="Applied Font",X="borderWidth",Y="borderVisibility",Z="borderFill",ba=0xFF797979,bb="cornerRadius",bc="cornerVisibility",bd="outerShadow",be="on",bf=false,bg="offsetX",bh=5,bi="offsetY",bj="blurRadius",bk="spread",bl="r",bm=0,bn="g",bo="b",bp="a",bq=0.349019607843137,br="adaptiveStyles",bs="interactionMap",bt="diagram",bu="objects",bv="masters",bw="objectPaths";
+return _creator();
+})());
